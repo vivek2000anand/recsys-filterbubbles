561975fc4003366158fd7104c9a640293d919ec1
Vivek Anand <vivek2000anand@gmail.com>
2021-11-01 23:51:33 -0400

    lets test tracin

---
+++ tracin/tracin.py
@@ -0,0 +1,101 @@
+import torch
+from torch._C import float32
+from copy import deepcopy
+
+def save_tracin_checkpoint(model, epoch, loss, optimizer, path):
+    """Saves a checkpoint for tracin to a path
+
+    Args:
+        model ([type]): [description]
+        epoch ([type]): [description]
+        loss ([type]): [description]
+        optimizer ([type]): [description]
+        path ([type]): [description]
+    """
+    torch.save({
+        'epoch': epoch,
+        'model_state_dict': model.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict(),
+        'loss': loss
+    }, path)
+    return
+
+def load_tracin_checkpoint(model, optimizer, path):
+    """Loads a tracin checkpoint from a path
+
+    Args:
+        model ([type]): [description]
+        optimizer ([type]): [description]
+        path ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """
+    checkpoint = torch.load(path)
+    model.load_state_dict(checkpoint['model_state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    epoch = checkpoint['epoch']
+    loss = checkpoint['loss']
+    return model, optimizer, epoch, loss
+
+def calculate_tracin_influence(model, source, source_label, target, target_label, optimizer, criterion, paths):
+    """Calculates influence of source on target datapoint based on TracIn method from checkpoints
+
+    Args:
+        model ([type]): [description]
+        source ([type]): [description]
+        target ([type]): [description]
+        optimizer ([type]): [description]
+        criterion ([type]): [description]
+        paths ([type]): [description]
+    """
+    num_checkpoints = len(paths)
+    influence = 0
+    for model_index in range(num_checkpoints):
+        # TODO get the initialization ready
+        # TODO add get gradients to the model
+        # Load the models and get the informations
+        curr_model = model()
+        curr_model, model_optimizer, model_epoch, _ = load_tracin_checkpoint(curr_model, deepcopy(optimizer), paths[model_index])
+        lr = get_lr(model_optimizer)
+        # Get source gradients 
+        model_optimizer.zero_grad()
+        source_outputs = curr_model(source)
+        source_loss = criterion(source_outputs, source_label)
+        source_loss.backward()
+        source_gradients = curr_model.get_gradients()
+        # Get target gradients
+        model_optimizer.zero_grad()
+        target_outputs = curr_model(target)
+        target_loss = criterion(target_outputs, target_label)
+        target_loss.backward()
+        target_gradients = curr_model.get_gradients()
+        # Calculate influence for this epoch. Flatten weights and dot product.
+        val = torch.dot(torch.flatten(source_gradients, target_gradients))
+        influence += val
+    return influence
+
+        
+
+        
+
+
+
+
+        
+        
+
+    return 
+
+def get_lr(optimizer):
+    """Gets learning rate given an optimizer
+
+    Args:
+        optimizer ([type]): [description]
+    """
+    for param_group in optimizer.param_groups:
+        return param_group['lr']
+
+
+if __name__ == "__main__":
+    print("This doesn't do jack")
---
+++ tracin/tracin_test.py
@@ -0,0 +1,54 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as transforms
+import torch.optim as optim
+
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+        # 
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+    def get_gradients(self):
+        """Get gradients for tracin method. (Do not call individually)
+        """
+
+        return
+
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+batch_size = 4
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                          shuffle=True, num_workers=2)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                         shuffle=False, num_workers=2)
+
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+model = Net()
+print(model.parameters())
