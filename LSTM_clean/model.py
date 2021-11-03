6efb52529fdfe59e8265c50389d61fe529320529
Vivek Anand <vivek2000anand@gmail.com>
2021-11-03 14:40:10 -0400

    Getting the LSTM pipeline ready!

---
+++ LSTM_clean/model.py
@@ -0,0 +1,134 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+from torch.autograd import Variable
+from torch import optim
+import numpy as np
+import math, random
+import os
+import time
+import copy
+
+class LSTM(nn.Module):
+    def __init__(self, input_size, output_size, hidden_dim, n_layers=1, device="cpu"):
+        super(LSTM, self).__init__()
+        
+        self.num_items = output_size
+        self.device = device 
+        self.emb_length = input_size
+        self.item_emb = nn.Embedding(self.num_items, self.emb_length,padding_idx=0)
+        self.batch_size = 1024
+        
+        # Defining some parameters
+        self.hidden_dim = hidden_dim
+        self.n_layers = n_layers
+
+        # Defining the layers
+        # LSTM Layer
+        self.LSTM = nn.LSTM(input_size, hidden_dim, n_layers, batch_first=True)
+        # Fully connected layer
+        self.fc = nn.Linear(hidden_dim, output_size)
+
+    def forward(self, x):
+        batch_size = x.size(0)
+        
+        # Initializing hidden state for first input using method defined below
+        hidden = self.init_hidden(batch_size)
+
+        # Passing in the input and hidden state into the model and obtaining outputs
+        out, hidden = self.LSTM(x, hidden)
+
+        # Reshaping the outputs such that it can be fit into the fully connected layer
+        inp = out[:, -1, :].contiguous().view(-1, self.hidden_dim)
+        out = self.fc(inp)
+
+        return out, hidden
+
+    def init_hidden(self, batch_size):
+        # This method generates the first hidden state of zeros which we'll use in the forward pass
+        # We'll send the tensor holding the hidden state to the device we specified earlier as well
+        hidden = (torch.zeros(self.n_layers, batch_size, self.hidden_dim).to(self.device).detach(), torch.zeros(self.n_layers, batch_size, self.hidden_dim).to(self.device).detach())
+        return hidden
+
+    def compute_metrics(self,test,test_labels):
+        """Computes how well the model performs on the test set.
+
+        Args:
+            test ([type]): [description]
+            test_labels ([type]): [description]
+
+        Returns:
+            [type]: [description]
+        """
+        test_num = len(test)
+        MRR,HITS,loss =0,0,0
+        probs = {}
+        criterion = nn.CrossEntropyLoss()
+        for iteration in range(int(test_num / self.batch_size) + 1):
+            st_idx, ed_idx = iteration * self.batch_size, (iteration + 1) * self.batch_size
+            if ed_idx > test_num:
+                ed_idx = test_num
+            output, hidden = self.forward(torch.stack([test[i][0] for i in range(st_idx,ed_idx)],dim=0).detach())
+            loss += criterion(output, test_labels[st_idx:ed_idx]).item()
+            output1 = output.view(-1, self.num_items)
+            prob = nn.functional.softmax(output1, dim=1).data.cpu()
+            np_prob = prob.numpy()
+            current_val = np.zeros((np_prob.shape[0],1))
+            for i in range(st_idx,ed_idx):
+                current_test_label = test[i][1]
+                current_val[i-st_idx,0] = np_prob[i-st_idx,current_test_label]
+            
+            new_prob = np_prob - current_val
+            ranks = np.count_nonzero(new_prob>0,axis=1)
+            
+            for i in range(st_idx,ed_idx):
+                predicted_rank = ranks[i-st_idx]+1 
+                MRR += 1/predicted_rank
+                HITS += (1 if predicted_rank<=10 else 0)
+                probs[i] = np_prob[i-st_idx,:]
+        # print("Outputs shape is ", output.size())
+        # print("outputs1 shape is ", output1.size())
+        return MRR/test_num,HITS/test_num,loss/test_num,probs
+
+    def traintest(self, train, test,epochs):
+        
+        train_num,test_num = len(train),len(test)
+        train_labels,test_labels = [],[]
+
+        for i in range(train_num):
+            train[i][0] = self.item_emb(torch.LongTensor(train[i][0]).to(self.device))
+            train_labels.append(train[i][1])
+        train_labels = torch.LongTensor(train_labels).to(self.device)
+         
+        for i in range(test_num):
+            test[i][0] = self.item_emb(torch.LongTensor(test[i][0]).to(self.device))
+            test_labels.append(test[i][1])
+        test_labels = torch.LongTensor(test_labels).to(self.device)
+
+        print("train # = {}\ttest # = {}".format(train_num,test_num))
+
+        criterion = nn.CrossEntropyLoss()
+        learning_rate = 1e-4
+        optimizer = optim.SGD(self.parameters(), lr=learning_rate, momentum=0.9)
+
+        start_time = time.time()
+        for epoch in range(epochs):
+            train_loss=0
+            for iteration in range(int(train_num/self.batch_size)+1):
+                st_idx,ed_idx = iteration*self.batch_size, (iteration+1)*self.batch_size
+                if ed_idx>train_num:
+                    ed_idx = train_num
+                        
+                optimizer.zero_grad()
+                output, hidden = self.forward(torch.stack([train[i][0] for i in range(st_idx,ed_idx)],dim=0).detach())
+                loss = criterion(output, train_labels[st_idx:ed_idx])
+                loss.backward()
+                train_loss += loss.item()
+                optimizer.step()
+                 
+            if epoch % 5 == 0:
+                test_MRR,test_HITS,test_loss,test_prediction = self.compute_metrics(test,test_labels)
+                print("Epoch {}\tTrain Loss: {}\tTest MRR: {}\tTest Recall@10: {}\tElapsed time: {}".format(epoch, train_loss/train_num,test_MRR,test_HITS, time.time() - start_time))
+                start_time = time.time()
+        
+        return test_prediction
---
+++ LSTM_clean/train_loop.py
@@ -0,0 +1,118 @@
+# In[1]
+import torch
+import torch.nn as nn
+import numpy as np
+import pandas as pd
+import copy
+import os
+
+from model import LSTM
+
+# In[2]
+def train_test_split(data=[]):
+    
+    """\
+    Description:
+    ------------
+        Sort by each user's interation. Tag train & test & (valid) set
+    """
+
+    (users,counts) = np.unique(data[:,0],return_counts = True)
+    users = users[counts>=10]
+
+    sequence_dic,pert_dic =  {int(user):[] for user in set(data[:,0])}, {int(user):[] for user in set(data[:,0])}
+    
+    user_dic = {int(user):idx for (idx,user) in enumerate(users)}
+    new_data = []
+    for i in range(data.shape[0]):
+        if int(data[i,0]) in user_dic:
+            new_data.append([int(data[i,0]),int(data[i,1]),data[i,2],0])
+
+    new_data = np.array(new_data)
+
+    for i in range(new_data.shape[0]):
+        sequence_dic[int(new_data[i,0])].append([i,int(new_data[i,1]),new_data[i,2]])
+    
+    for user in sequence_dic.keys():
+        cur_test = int(0.1*len(sequence_dic[user]))
+        for i in range(cur_test):
+            interaction = sequence_dic[user].pop()
+            new_data[interaction[0],3] = 2
+
+        #cur_val = int(0.1*len(sequence_dic[user]))
+        #for i in range(cur_val):
+        #    interaction = sequence_dic[user].pop()
+        #    new_data[interaction[0],3] = 1
+
+    return new_data
+
+def sequence_generator(data, look_back = 50):
+
+    """\
+    Description:
+    ------------
+        Input data for LSTM: Convert to user trajectory (maximum length: look back)
+    """
+
+    train,test, valid = [],[],[]
+    unique_users = set(data[:,0])
+    items_per_user = {int(user):[0 for i in range(look_back)] for user in unique_users}
+    
+    for (idx,row) in enumerate(data):
+      user,item,time = int(row[0]),int(row[1]),row[2]
+      items_per_user[user] = items_per_user[user][1:]+[item+1]
+      current_items = items_per_user[user]
+      if row[3]==0:
+        train.append([current_items[:-1],current_items[-1]])
+      elif row[3]==2:
+        test.append([current_items[:-1],current_items[-1]])
+      else:
+        valid.append([current_items[:-1],current_items[-1]])
+                                                                
+    return train,test #,valid
+
+
+# In[3] Set CUDA env
+epochs = 2
+
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"]='0'
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print("Device is ", device)
+
+
+# In[4] Load data (ex: wikipedia [user, item, timestamp])
+print("Loading data")
+path = "../data/"
+raw_data = pd.read_csv(path + "wikipedia.tsv", sep='\t', header=None)
+data = raw_data.values[:,-3:]
+look_back = 50
+
+unique_users = sorted(list(set(data[:, 0])))
+unique_items = sorted(list(set(data[:, 1])))
+user_dic = {user:idx for (idx,user) in enumerate(unique_users)}
+item_dic = {item:idx for (idx,item) in enumerate(unique_items)}
+
+for (idx, row) in enumerate(data):
+    user,item,time = user_dic[row[0]],item_dic[row[1]],row[2]
+    data[idx,0],data[idx,1] = int(user),int(item)
+
+
+# In[5] Data preprocessing
+print("Preprocessing Data")
+original_data = train_test_split(data=data)
+(train,test) = sequence_generator(original_data,look_back)
+test_ground_truth = {i:test[i][1] for i in range(len(test))}
+
+print("Train: {}, Test: {}".format(len(train),len(test)))
+
+# In[6] Run LSTM
+model = LSTM(input_size=128, output_size=len(unique_items)+1, hidden_dim=64, n_layers=1, device=device).to(device)
+model.LSTM.flatten_parameters()
+print("Model is ", model)
+print("Training and testing")
+original_prediction = model.traintest(train=train,test=test,epochs = epochs)
+print("Finished")
+
+# %%
