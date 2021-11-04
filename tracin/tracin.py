4bea8679f5e20505ef68dd03151c86bcf01217b2
Vivek Anand <vivek2000anand@gmail.com>
2021-11-04 14:13:16 -0400

    Adding tracin saving for LSTM on twitch: 150 epochs

--- tracin/tracin.py
+++ tracin/tracin.py
@@ -67,7 +67,8 @@ def calculate_tracin_influence(model, source, source_label, target, target_label
         # print("LR is ", lr)
         # Get source gradients 
         model_optimizer.zero_grad()
-        source_outputs, _ = curr_model.forward(source)
+        source_outputs, hidden1 = curr_model.forward(source)
+        hidden1.detach()
         # print("Source outputs are ", source_outputs, source_outputs[0].shape)
         # print("first element", source_outputs[0])
         # print("Source label is ", source_label)
@@ -77,7 +78,8 @@ def calculate_tracin_influence(model, source, source_label, target, target_label
         source_gradients = curr_model.get_gradients()
         # Get target gradients
         model_optimizer.zero_grad()
-        target_outputs, _ = curr_model.forward(target)
+        target_outputs, hidden2 = curr_model.forward(target)
+        hidden2.detach()
         # print("target outputs are ", target_outputs)
         target_loss = criterion(target_outputs[0:1], target_label)
         # print("target loss is ", target_loss)
