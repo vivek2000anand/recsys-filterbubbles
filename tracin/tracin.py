42088bf0fa4fdc290862c458a714e5b680c1d45a
Vivek Anand <vivek2000anand@gmail.com>
2021-11-04 00:44:30 -0400

    Adding tracin saving for LSTM on twitch: 150 epochs

--- tracin/tracin.py
+++ tracin/tracin.py
@@ -58,7 +58,7 @@ def calculate_tracin_influence(model, source, source_label, target, target_label
         # TODO add get gradients to the model
         # Load the models and get the informations
         curr_model = model(input_size=128, output_size=5673, hidden_dim=64, n_layers=1) 
-        model.flatten()
+        curr_model.flatten()
         optimizer = SGD(curr_model.parameters(), lr=5e-2, momentum=0.9)
         curr_model, model_optimizer, _, _ = load_tracin_checkpoint(curr_model,optimizer, paths[model_index])
         lr = get_lr(model_optimizer)
