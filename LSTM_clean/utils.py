67419a3a83b890df738bc1814586b01d5b663ec4
matthewyangcs <matthewyangcs@gmail.com>
2021-11-30 07:27:57 +0000

    refactored train_valid_test_split

--- LSTM_clean/utils.py
+++ LSTM_clean/utils.py
@@ -7,55 +7,51 @@ def printl(length=80):
     print(length * "-")
 
 
-def train_valid_test_split(data):
+def train_valid_test_split(
+    data, min_items_per_user=10, train_cutoff=0.8, valid_cutoff=0.9
+):
     """Takes in NX3 array and returns sequence-like data
-    
+
     The data argument should be formatted as:
         Col 1: User ID
         Col 2: Item ID
         Col 3: Time
+
+    Args:
+        min_items_per_user: Only keep users with >= this # number of interactions
+        train_cutoff: [0, train_cutoff] will be labeled as training data
+        valid_cutoff: (train_cutoff, valid_cutoff] will be labeled as validation data
+                    : (valid_cutoff, 1.0] will be labeled as testing data
+
     """
-    # Filter out users with < 10
-    (users, counts) = np.unique(data[:, 0], return_counts=True)
-    users = users[counts >= 10]
-
-    # sequence_dictt = {int(user): [] for user in set(data[:, 0])}
-    sequence_dict = defaultdict(list)
-
-    # sequence_dict, pert_dic = {int(user): [] for user in set(data[:, 0])}, {
-    #     int(user): [] for user in set(data[:, 0])
-    # }
-
-    
-    user_dic = {int(user): idx for (idx, user) in enumerate(users)}
+    # Only keep users with >= threshold items
+    users, counts = np.unique(data[:, 0], return_counts=True)
+    total_items_per_user = {
+        user: count for user, count in zip(users, counts) if count >= min_items_per_user
+    }
+    count_per_user = defaultdict(int)
 
     new_data = []
-    for i in range(data.shape[0]):
-        user, item, time = data[i]
-        # if int(data[i, 0]) in user_dic:
-        #     new_data.append([int(data[i, 0]), int(data[i, 1]), data[i, 2], 0])
+    for user, item, time in data:
+        if user not in total_items_per_user:
+            continue
+        else:
+            total_items = total_items_per_user[user]
 
-        if user in user_dic:
+        count_per_user[user] += 1
+
+        if count_per_user[user] <= train_cutoff * total_items:
             new_data.append([user, item, time, 0])
+        elif count_per_user[user] <= valid_cutoff * total_items:
+            new_data.append([user, item, time, 1])
+        elif count_per_user[user] <= total_items:
+            new_data.append([user, item, time, 2])
+        else:
+            raise ValueError(
+                "There's a bug in the counting of total items or curr items!"
+            )
 
-    new_data = np.array(new_data)
-
-    for i in range(new_data.shape[0]):
-        user, item, time = new_data[i]
-        sequence_dict[user].append([i, item, time])
-
-    for user in sequence_dict.keys():
-        cur_test = int(0.05 * len(sequence_dict[user]))
-        for i in range(cur_test):
-            interaction = sequence_dict[user].pop()
-            new_data[interaction[0], 3] = 2
-
-        # cur_val = int(0.1*len(sequence_dict[user]))
-        # for i in range(cur_val):
-        #    interaction = sequence_dict[user].pop()
-        #    new_data[interaction[0],3] = 1
-
-    return new_data
+    return np.array(new_data)
 
 
 # NOTE: Sequence generator already increases item ids by 1!
@@ -67,22 +63,23 @@ def sequence_generator(data, look_back=50):
         Input data for LSTM: Convert to user trajectory (maximum length: look back)
     """
 
-    train,valid,test = [],[],[]
-    unique_users = set(data[:,0])
-    items_per_user = {int(user):[0 for i in range(look_back)] for user in unique_users}
-    
-    for (idx,row) in enumerate(data):
-        user,item,time = int(row[0]),int(row[1]),row[2]
-        items_per_user[user] = items_per_user[user][1:]+[item+1]
+    train, valid, test = [], [], []
+    unique_users = set(data[:, 0])
+    items_per_user = {int(user): [0 for i in range(look_back)] for user in unique_users}
+
+    for (idx, row) in enumerate(data):
+        user, item, time = int(row[0]), int(row[1]), row[2]
+        # The item id increase happens here
+        items_per_user[user] = items_per_user[user][1:] + [item + 1]
         current_items = items_per_user[user]
-        if row[3]==0:
-            train.append([current_items[:-1],current_items[-1]])                                                                                            
-        elif row[3]==1:
-            valid.append([current_items[:-1],current_items[-1]])
+        if row[3] == 0:
+            train.append([current_items[:-1], current_items[-1]])
+        elif row[3] == 1:
+            valid.append([current_items[:-1], current_items[-1]])
         else:
-            test.append([current_items[:-1],current_items[-1]])
-                                                                
-    return train,valid,test
+            test.append([current_items[:-1], current_items[-1]])
+
+    return train, valid, test
 
 
 def get_diversity(prev_item_communities, predicted_item_communities, bounds=0.1):
