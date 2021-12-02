1fab8e48d260e474f26e8ad2bc8c8e71a5fa6a5d
Vivek Anand <vivek2000anand@gmail.com>
2021-12-01 23:59:23 -0500

    Trying the batching

--- diversity_experiments.py
+++ diversity_experiments.py
@@ -152,6 +152,6 @@ for i in range(len(total_diversity)):
 # Compares Effects of filter bubbles on the diverse points
 #influences = run_experiments(LSTM, sources=filter_bubbles, targets=diverse_points, sources_labels=filter_bubbles_labels,
 #targets_labels=diverse_points_labels, paths=checkpoints, device=device)
-influences = run_experiments(LSTM, sources=filter_bubbles, targets=diverse_points, sources_labels=filter_bubbles_labels,
+influences = calculate_tracin_influence_batch(LSTM, sources=filter_bubbles, targets=diverse_points, sources_labels=filter_bubbles_labels,
 targets_labels=diverse_points_labels, paths=checkpoints, device=device)
 print("Summary Statistics for influences: \n", pd.Series(influences).describe())
