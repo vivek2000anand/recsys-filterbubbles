21ab630f378adbf8a089b824ceb1702504a63bc6
Vivek Anand <vivek2000anand@gmail.com>
2021-12-07 01:09:55 -0500

    Reducing subset significantly

--- length_tests.py
+++ length_tests.py
@@ -24,9 +24,9 @@ from copy import deepcopy
 import time
 
 OUTPUT_SIZE = 3312
-NUM_TRAIN_SAMPLES = 25
+NUM_TRAIN_SAMPLES = 100
+NUM_VAL_SAMPLES = 10
 NUM_REPETITIONS = 20
-NUM_SUBSET = 1000
 STEP_SIZE = 5
 
 def get_checkpoints():
@@ -57,19 +57,19 @@ def get_train_validation():
     print(f"Validation set is length: {len(valid)}")
     return train, train_labels, valid, valid_labels
 
-def get_train_subset(length, x, x_labels, train_lengths, num_sample=100, subset_size=10000, seed=10):
+def get_train_subset(length, x, x_labels, train_lengths):
     x_subset =[]
     x_labels_subset = []
     for i in range(len(valid)):
         if train_lengths[i] == length:
             x_subset.append(x[i])
             x_labels_subset.append(x_labels[i])
-    x_subset, _, x_labels_subset, _ = train_test_split(x_subset, x_labels_subset, train_size=num_sample, random_state=seed)
-    x_subset, x_labels_subset = shuffle(x_subset, x_labels_subset, random_state=seed)
-    if len(x_subset) > subset_size:
-        return x_subset[:subset_size], x_labels_subset[:subset_size]
-    else:
-        return x_subset, x_labels_subset
+    # x_subset, _, x_labels_subset, _ = train_test_split(x_subset, x_labels_subset, train_size=num_sample, random_state=seed)
+    # x_subset, x_labels_subset = shuffle(x_subset, x_labels_subset, random_state=seed)
+    # if len(x_subset) > subset_size:
+    #     return x_subset[:subset_size], x_labels_subset[:subset_size]
+    # else:
+    return x_subset, x_labels_subset
 
 def get_length(data_point):
     for i in range(len(data_point)):
@@ -78,12 +78,16 @@ def get_length(data_point):
     return 0
 
 
-def get_points(x, x_label, y, y_label):
+def get_points(x, x_label, y, y_label,x_num_sample =100, y_num_sample=10, seed=69):
+    x, x_label = shuffle(x, x_label, random_state= seed)
+    y, y_label = shuffle(y, y_label, random_state=seed)
+    x, x_label = x[:x_num_sample], x_label[:x_num_sample]
+    y, y_label = y[:y_num_sample], y_label[:y_num_sample]
     combos = list(product(zip(x, x_label), zip(y, y_label)))
-    sources = [c[0][0] for c in combos]
-    source_labels = [c[0][1] for c in combos]
-    targets = [c[1][0] for c in combos]
-    target_labels = [c[1][1] for c in combos]
+    sources = [x[0][0] for c in combos]
+    source_labels = [x[0][1] for c in combos]
+    targets = [y[1][0] for c in combos]
+    target_labels = [y[1][1] for c in combos]
     print(f"Number of datapoints {len(sources)}")
     return sources, source_labels, targets, target_labels
 
@@ -105,10 +109,10 @@ for h in range(NUM_REPETITIONS):
     print(f"Starting outer loop with {h}")
     for i in range(0, 50, STEP_SIZE):
         start_length_time = time.time()
-        train_subset, train_labels_subset = get_train_subset(i, train, train_labels, train_lengths, num_sample=NUM_TRAIN_SAMPLES, seed=h)
+        train_subset, train_labels_subset = get_train_subset(i, train, train_labels, train_lengths)
         if len(train_subset) != 0:
             print("About to cartesian product")
-            sources, source_labels, targets, target_labels = get_points(train, train_labels, valid, valid_labels)
+            sources, source_labels, targets, target_labels = get_points(train, train_labels, valid, valid_labels, x_num_sample=NUM_TRAIN_SAMPLES, y_num_sample=NUM_VAL_SAMPLES, seed=h)
             print("About to tracin")
             influence = approximate_tracin_batched(LSTM, sources=sources, targets=targets, source_labels=source_labels, target_labels=train_labels, optimizer="SGD", paths=checkpoints, batch_size=4000, num_items=OUTPUT_SIZE, device=device)
             influences[i]= influences[i].append(influence)
