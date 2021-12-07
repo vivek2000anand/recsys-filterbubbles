ba4768fdf4c89bacab7012292c8a3ed16146f7bd
Vivek Anand <vivek2000anand@gmail.com>
2021-12-06 20:25:41 -0500

    Adding length test

--- length_tests.py
+++ length_tests.py
@@ -3,8 +3,10 @@ import pickle
 import os
 
 cwd = os.getcwd()
-path = cwd + "/data/test.data"
-test_data = np.load(path, allow_pickle=True)
-print(test_data)
-# valid_data = np.load(path, allow_pickle=True)
+test_path = cwd + "/data/test.data"
+train_path = cwd + "/data/train.data"
+valid_path = cwd + "/data/valid.data"
+
+train_data = np.load(train_path, allow_pickle=True)
+valid_data = np.load(valid_path, allow_pickle=True)
 # test_data = np.load(path, allow_pickle=True)
